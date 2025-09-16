52e12b508359d6aaed04f36896b692533d01b3f1
Tommymuia <muiathomas.mt@gmail.com>
2025-09-16 09:01:18 +0300

    added

--- index.js
+++ index.js
@@ -1,16 +1,36 @@
 const tutorials = [
   'what does the this keyword mean?',
-  'What is the Constructor OO pattern?',
-  'implementing Blockchain Web API',
-  'The Test Driven Development Workflow',
-  'What is NaN and how Can we Check for it',
-  'What is the difference between stopPropagation and preventDefault?',
-  'Immutable State and Pure Functions',
+  'what is the constructor oo pattern?',
+  'implementing blockchain web api',
+  'the test driven development workflow',
+  'what is nan and how can we check for it',
+  'what is the difference between stoppropagation and preventdefault?',
+  'immutable state and pure functions',
   'what is the difference between == and ===?',
   'what is the difference between event capturing and bubbling?',
-  'what is JSONP?'
+  'what is jsonp?'
 ];
 
-const titleCased = () => {
-  return tutorials
+function titleCased() {
+  return tutorials.map(tutorial => {
+    return tutorial
+      .split(" ")
+      .map(word => {
+        const lower = word.toLowerCase();
+        if (lower === "oo") return "OO";
+        if (lower === "api") return "API";
+        if (lower === "nan") return "NaN";
+        if (lower === "jsonp?") return "JSONP?";
+
+       
+        if (lower === "stoppropagation") return "StopPropagation";
+        if (lower === "preventdefault?") return "PreventDefault?"; 
+        if (lower === "preventdefault") return "PreventDefault";
+
+        
+        return word[0].toUpperCase() + word.slice(1);
+      })
+      .join(" ");
+  });
 }
+
